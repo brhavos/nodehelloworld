dbe8f72d41fa48fcc0d8bc6249d3ab2e4fd65bac
brhavos <brhavos@gmail.com>
2022-02-07 16:10:38 -0300

    clonei um outro projeto

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-app.listen(process.env.PORT || 3000);
-console.log("Olá Mundo!");
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World! Carlos, seja bem vindo ao Node.JS, Heroku & Cia!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
