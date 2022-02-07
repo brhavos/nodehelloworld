859fe7000c9fe40cf3cc4468dc04e52cf1fad0a1
brhavos <brhavos@gmail.com>
2022-02-07 16:55:04 -0300

    ajustes finos

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Tirei o readme...')
+  response.send("Hello World!" + "\" + "by Carlos Henrique")
 })
 
 app.listen(app.get('port'), function() {
